bace814a85e5519db93b07bdbb7fcd36f03b5bf5
dddddavy <1500012780@pku.edu.cn>
2017-07-13 20:19:54 +0800

    Add files via upload

---
+++ cannot_touch/README.md
@@ -0,0 +1,4 @@
+## cannot touch
+
+the script will make you unable to press the search button of baidu.com
+
---
+++ cannot_touch/js/touch.js
@@ -0,0 +1,22 @@
+function btn_move(ele, mouseLeft, mouseTop){
+    var leftRnd = (Math.random() - 0.5) * 20;
+    var topRnd = (Math.random() - 0.5) * 20;
+    var btnLeft = mouseLeft + (leftRnd > 0 ? 100 : -100) + leftRnd;
+    var btnTop = mouseTop + (topRnd > 0 ? 30 : -30) + topRnd;
+    btnLeft = btnLeft < 100 ? (btnLeft + window.innerWidth - 200) : 
+        (btnLeft > window.innerWidth - 100 ? btnLeft - window.innerWidth + 200 : btnLeft);
+    btnTop = btnTop < 100 ? (btnTop + window.innerHeight - 200) : 
+        (btnTop > window.innerHeight - 100 ? btnTop- window.innerHeight + 200 : btnTop);
+    ele.style.position = 'fixed';
+    ele.style.left = btnLeft+'px';
+    ele.style.top = btnTop+'px';
+}
+
+function over_btn(e) {
+    if (!e) {
+        e = window.event();
+    }
+    btn_move(this, e.clientX, e.clientY)
+}
+
+document.getElementById('su').onmouseover = over_btn;
